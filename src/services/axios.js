9a2d40aee7fdc79efb846d09427fc4e6337df4e3
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2025-07-14 21:14:02 -0400

    Update axios.js

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { EXPO_PUBLIC_API_LOCAL, EXPO_PUBLIC_API_PROD } from '@env';
 
 const axiosInstance = axios.create({
-  baseURL: EXPO_PUBLIC_API_LOCAL,
+  baseURL: EXPO_PUBLIC_API_PROD,
   headers: {
     'Content-Type': 'application/json',
   },
