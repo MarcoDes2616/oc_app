228b42ef9f3cf4e7d1ea40d226d95badead0e937
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2025-08-07 10:15:47 -0400

    Update MainNavigator.jsx

--- src/navigation/MainNavigator.jsx
+++ src/navigation/MainNavigator.jsx
@@ -1,56 +1,64 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import SenalesScreen from '../screens/SenalesScreen';
-import PerfilScreen from '../screens/PerfilScreen';
-import DashboardScreen from '../screens/DashboardScreen';
-import NotificationFabButton from '../components/NotificationFabButton';
+import SenalesScreen from "../screens/SenalesScreen";
+import PerfilScreen from "../screens/PerfilScreen";
+import DashboardScreen from "../screens/DashboardScreen";
+import NotificationFabButton from "../components/NotificationFabButton";
 
 const Tab = createBottomTabNavigator();
 
 const MainNavigator = () => {
   return (
     <>
-      <NotificationFabButton />
-    
-    <Tab.Navigator
-      initialRouteName="Senales"
-      screenOptions={{
-        headerShown: true,
-        tabBarActiveTintColor: '#6200ee',
-      }}
-    >
-      <Tab.Screen
-        name="Senales"
-        component={SenalesScreen}
-        options={{
-          tabBarLabel: 'Señales',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="chart-line" color={color} size={size} />
-          ),
+      {/* <NotificationFabButton /> */}
+
+      <Tab.Navigator
+        initialRouteName="Senales"
+        screenOptions={{
+          headerShown: true,
+          tabBarActiveTintColor: "#6200ee",
         }}
-      />
-      <Tab.Screen
-        name="Dashboard"
-        component={DashboardScreen}
-        options={{
-          tabBarLabel: 'Dashboard',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="wallet" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Perfil"
-        component={PerfilScreen}
-        options={{
-          tabBarLabel: 'Perfil',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account-cog" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+      >
+        <Tab.Screen
+          name="Senales"
+          component={SenalesScreen}
+          options={{
+            tabBarLabel: "Señales",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="chart-line"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Dashboard"
+          component={DashboardScreen}
+          options={{
+            tabBarLabel: "Dashboard",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="wallet" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Perfil"
+          component={PerfilScreen}
+          options={{
+            tabBarLabel: "Perfil",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="account-cog"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </>
   );
 };
