8792ea4674b7bce04b6acb59717142c61986aaaf
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2025-08-07 18:34:37 -0400

    Update SettingsScreen.jsx

--- src/screens/Profile/SettingsScreen.jsx
+++ src/screens/Profile/SettingsScreen.jsx
@@ -1,11 +1,24 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../../context/AppContext';
 import { View, Text } from 'react-native';
 import ProfileMenu from '../../components/ProfileMenu';
+import BiometricToggle from "../../components/BiometricToggle";
+import { Button } from 'react-native-paper';
 
 const SettingsScreen = () => {
+  const { logout } = useContext(AppContext);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <ProfileMenu />
-      <Text>Configuración de la App</Text>
+      <Text style={{ marginBottom: 20 }}>⚙️ Perfil y configuración</Text>
+
+      {/* Aquí el bi-state */}
+      <BiometricToggle />
+
+      <Button mode="outlined" style={{ marginTop: 30 }} onPress={logout}>
+        Cerrar sesión
+      </Button>
     </View>
   );
 }
